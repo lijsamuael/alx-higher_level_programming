c2b09ac14579b15f3f6aadcb5cc04bc1bc217c95
Samuael Ketema <lijsamuael@gmail.com>
2022-11-30 01:47:46 +0300

    adding the eith task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if c.islower():
+    char = ord(c)
+    if char >= 97 and char <= 122:
         return True
-    else:
-        return False
+    return False
