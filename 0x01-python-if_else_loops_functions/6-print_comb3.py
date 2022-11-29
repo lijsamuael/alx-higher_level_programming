6442b914b22b247c4d1c4d845f229b5281c6ed4f
Samuael Ketema <lijsamuael@gmail.com>
2022-11-30 01:26:15 +0300

    adding the ninth task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if j >= i and i != j:
+            if i ==8 and j == 9:
+                print(89)
+            else:
+                print("{}".format(str(i) + str(j)), end=", ")
