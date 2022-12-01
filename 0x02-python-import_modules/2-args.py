399b32206a3b8ea9c96eb8af0b0098fedb43efc9
Samuael Ketema <lijsamuael@gmail.com>
2022-12-02 01:18:13 +0300

    making correction on the code style

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    argc  = len(sys.argv)
+    argc = len(sys.argv)
     if argc - 1 == 0:
         print("{} arguments.".format(argc - 1))
     elif argc - 1 == 1:
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     else:
         print("{} arguments:".format(argc - 1))
     for i in range(1, argc):
-        print("{}: {}".format(i,sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
