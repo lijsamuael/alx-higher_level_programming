9018e1c6703f286bce276c5f704311ebb2816545
Samuael Ketema <lijsamuael@gmail.com>
2022-12-02 01:14:58 +0300

    addding the third task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+argc  = len(sys.argv)
+if argc - 1 == 0:
+    print("{} arguments.".format(argc - 1))
+elif argc - 1 == 1:
+    print("{} argument:".format(argc - 1))
+else:
+    print("{} arguments:".format(argc - 1))
+for i in range(1, argc):
+    print("{}: {}".format(i,sys.argv[i]))
