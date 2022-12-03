5a0f3aba7607a19dafa8e041f9065f26dac5f734
Samuael Ketema <lijsamuael@gmail.com>
2022-12-03 14:53:48 +0300

    fixing an error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in my_list:
+    reversed_list = my_list[::-1]
+    for i in reversed_list:
         print("{:d}".format(i))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    print(my_string.translate({ord(i): None for i in "Cc"}))
+    print("{}".format(my_string.translate({ord(i): None for i in "Cc"})))
