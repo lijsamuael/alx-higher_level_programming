e05ad602d5332f8bfbef4550f9b140e688a672f0
Samuael Ketema <lijsamuael@gmail.com>
2022-12-19 19:24:42 +0300

    correcting the return statement

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -4,8 +4,8 @@ safe_print_list = __import__('0-safe_print_list').safe_print_list
 my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list(my_list, 2)
-print()
+print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
-print()
+print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
-
+print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for idx, el in enumerate(my_list):
-            if x == idx:
-                print()
+            if idx == x:
                 break
             print("{}".format(el), end="")
-
+            count += 1
     except IndexError:
-        pass
-    return idx
+        count = 5
+    print()
+    return count
