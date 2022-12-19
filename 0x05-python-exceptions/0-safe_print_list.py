ecd388e6260bcd6ffe49d87fe30eef2b2315e488
Samuael Ketema <lijsamuael@gmail.com>
2022-12-19 18:56:47 +0300

    a function that prints a number of elements from the list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for idx, el in enumerate(my_list):
-        if x == idx:
-            break
-        print("{:d}".format(el), end="")
+    try:
+        for idx, el in enumerate(my_list):
+            if x == idx:
+                break
+            print("{}".format(el), end="")
+        print()
+    except IndexError:
+        pass
+    return idx
