f609524f9b258a1f46c43511d32afc35ad6d9948
Samuael Ketema <lijsamuael@gmail.com>
2022-12-19 23:52:20 +0300

    correcting the pycode style

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-	try:
-		print("{:d}".format(value))
-	except ValueError:
-		return False
-	except:
-		return False
-	else:
-		return True
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
+    else:
+        return True
