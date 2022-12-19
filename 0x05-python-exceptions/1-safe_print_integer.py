32b5c6b447437745ef961533162c7ed21a4614d7
lijsamuael <lijsamuael@gmail.com>
2022-12-19 20:31:15 +0000

    fixing the stricutre jof the file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except:
-        return False
-    else:
-        return True
+	try:
+		print("{:d}".format(value))
+	except ValueError:
+		return False
+	except:
+		return False
+	else:
+		return True
