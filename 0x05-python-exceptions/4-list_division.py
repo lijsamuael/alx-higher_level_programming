7a0f42dff46ef7d6b468432b0f193c13812ea8f0
Samuael Ketema <lijsamuael@gmail.com>
2022-12-20 01:12:18 +0300

    a function that hanles valueerror typeerror and zerodivisionerror

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            answer = ( my_list_1[i] / my_list_2[i] )
+        except TypeError:
+            answer = 0
+            print("{}".format("wrong type"))
+        except ZeroDivisionError:
+            answer = 0
+            new_list.append(0)
+            print("{}".format("division by 0"))
+        except IndexError:
+            answer = 0
+            print("{}".format("out of range"))
+        finally:
+            new_list.append(answer)
+    return new_list
