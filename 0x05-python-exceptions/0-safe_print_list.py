2d06520196ce119adef1a1a69aad004211d07873
Samuael Ketema <lijsamuael@gmail.com>
2022-12-20 00:16:38 +0300

    a file that hanldes type error exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+#!/usr/bin/python4
+def safe_print_list(my_list=[], x=1):
     count = 0
     try:
         for idx, el in enumerate(my_list):
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-
+    count = 0
+    for el in range(x):
+        try:
+            print("{:d}".format(my_list[el]), end="")
+        except (TypeError, ValueError):
+            continue
+        else:
+            count += 1
+    print()
+    return count
