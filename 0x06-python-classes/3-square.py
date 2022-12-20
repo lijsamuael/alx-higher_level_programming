dbd03081df2deda7f15d9352213bfe710863789b
Samuael Ketema <lijsamuael@gmail.com>
2022-12-20 22:48:10 +0300

    a class that computes the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" a sqaure class"""
+
+
+class Square:
+    """ a class that defines private instance variables"""
+
+    _Square__size = None
+
+    def __init__(self, size=0):
+
+        """The constructor of the class"""
+
+        self._Square__size = size
+        if not isinstance(size, int):
+            raise (TypeError("size must be an integer"))
+        if size < 0:
+            raise (ValueError("size must be >= 0"))
+
+    def area(self):
+        return self._Square__size ** 2
