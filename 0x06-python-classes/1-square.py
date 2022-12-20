fa6d9dcbc72d6fb6d7c33cb11271516f264bf922
Samuael Ketema <lijsamuael@gmail.com>
2022-12-20 19:31:39 +0300

    a claass that has private attributes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a sqaure class"""
+
+class Square:
+    """ a class that defines private instance variables"""
+    _size = None
+    def __init__(self, size):
+        self._size = size
