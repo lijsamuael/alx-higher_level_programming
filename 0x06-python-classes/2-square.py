db97e3e817d34a881bbd3267c4550181b939d9a7
Samuael Ketema <lijsamuael@gmail.com>
2022-12-20 22:38:40 +0300

    a class that sets a value using init methos

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" a sqaure class"""
+
+
+class Square:
+    """ a class that defines private instance variables"""
+
+    _Square__size = None
+
+    def __init__(self, size=0):
+
+        """The constructor of the class"""
+
+        self._Square__size = size
+        if isinstance(size, int) == False:
+            raise(TypeError("size must be an integer"))
+        if size < 0:
+            raise(ValueError("size must be >= 0"))
