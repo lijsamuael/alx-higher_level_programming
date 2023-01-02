08d60de8ba0e34d6319b5d2b4977dcaf2c8a21e0
Samuael Ketema <lijsamuael@gmail.com>
2023-01-02 23:54:17 +0300

    fixing a problem

--- 0x08-python-more_classes/eneja.py
+++ 0x08-python-more_classes/eneja.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""class with private variables"""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Rectangle class"""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
+        """sth
+        width (int): width
+        height (int): height
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
-        return self.__width
+        """sth"""
+        return self._Rectangle_width
 
     @width.setter
     def width(self, value):
+        """sth"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self._Rectangle_width = value
 
     @property
     def height(self):
-        """Get/set the height of the rectangle."""
-        return self.__height
+        """sth"""
+        return self._Rectangle_height
 
     @height.setter
     def height(self, value):
+        """sth"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self._Rectangle_height = value
