ab26a848714199af424f02db014371db04850aa7
Samuael Ketema <lijsamuael@gmail.com>
2023-01-02 23:56:22 +0300

    fixing a problem

--- 0x08-python-more_classes/ene.py
+++ 0x08-python-more_classes/ene.py
@@ -1,42 +1,42 @@
 #!/usr/bin/python3
-"""class with private variables"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Rectangle class"""
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """sth
-        width (int): width
-        height (int): height
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """sth"""
-        return self._Rectangle_width
+        """Get/set the width of the rectangle."""
+        return self.__width
 
     @width.setter
     def width(self, value):
-        """sth"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._Rectangle_width = value
+        self.__width = value
 
     @property
     def height(self):
-        """sth"""
-        return self._Rectangle_height
+        """Get/set the height of the rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
-        """sth"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._Rectangle_height = value
+        self.__height = value
