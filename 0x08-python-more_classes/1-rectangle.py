7bacd59c2b23f1a414a9fb3c0972b8a71a18066b
Samuael Ketema <lijsamuael@gmail.com>
2023-01-02 23:10:46 +0300

    a class with setter and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,20 +6,28 @@ class Rectangle:
     """Rectangle class"""
 
     def __init__(self, width=0, height=0):
+        """sth"""
         self.width = width
         self.height = height
 
     def width(self):
+        """sth"""
         return self.width
+
     def width(self, value):
+        """sth"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >=0")
         self.width = value
+
     def height(self):
+        """sth"""
         return self.height
+
     def height(self, value):
+        """sth"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
