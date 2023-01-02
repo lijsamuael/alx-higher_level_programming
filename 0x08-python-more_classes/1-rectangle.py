7f87509e63ad68450555f97855a3157cceb4f189
Samuael Ketema <lijsamuael@gmail.com>
2023-01-02 23:17:33 +0300

    a class with setter and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,26 +10,30 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         """sth"""
-        return self._width
+        return self._Rectangle_width
 
+    @width.setter
     def width(self, value):
         """sth"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >=0")
-        self._width = value
+        self._Rectangle_width = value
 
+    @property
     def height(self):
         """sth"""
-        return self._height
+        return self._Rectangle_height
 
+    @height.setter
     def height(self, value):
         """sth"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self._Rectangle_height = value
