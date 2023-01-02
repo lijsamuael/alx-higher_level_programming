00048d542431ac61fb98001761b7af25158d16aa
Samuael Ketema <lijsamuael@gmail.com>
2023-01-02 23:58:28 +0300

    fixing a problem

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,17 +6,14 @@ class Rectangle:
     """Rectangle class"""
 
     def __init__(self, width=0, height=0):
-        """sth
-        width (int): width
-        height (int): height
-        """
+        """sth"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
         """sth"""
-        return self._width
+        return self.__Rectangle_width
 
     @width.setter
     def width(self, value):
@@ -25,12 +22,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__Rectangle_width = value
 
     @property
     def height(self):
         """sth"""
-        return self._height
+        return self.__Rectangle_height
 
     @height.setter
     def height(self, value):
@@ -39,4 +36,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__Rectangle_height = value
