8033ce4939c6a8b59dd28572dbc99774a3acefa4
Samuael Ketema <lijsamuael@gmail.com>
2023-03-13 23:33:53 +0300

    (fea) a file that convets to integetr

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2], 10);
-console.log(isNaN(num)? 'Not a number': `My number: ${num}`)
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
