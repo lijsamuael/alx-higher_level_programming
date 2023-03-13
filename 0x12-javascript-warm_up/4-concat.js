09a74dbb4c655aa611313c6cf7d50899d0b9e1c0
Samuael Ketema <lijsamuael@gmail.com>
2023-03-13 23:24:11 +0300

    (fea) adding  a file that prints the first two arguments passed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-const secondArg = process.argv[3];
-console.log(`${firstArg} is ${secondArg}`);
-
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
