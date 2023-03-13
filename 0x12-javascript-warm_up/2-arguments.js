8680f98d163a26ed51b9638ad41c77ffdf23860f
Samuael Ketema <lijsamuael@gmail.com>
2023-03-13 23:04:59 +0300

    (fea) adding  a file that prints based on the number of arguments passed to it

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-
-const args = process.argv;
-
-if (args.length === 2) {
-  console.log('No argument');
-} else if (args.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
-
+const count = process.argv.slice(2).length;
+console.log(count === 0 ? 'No argument' : count === 1 ? 'Argument found' : 'Arguments found');
