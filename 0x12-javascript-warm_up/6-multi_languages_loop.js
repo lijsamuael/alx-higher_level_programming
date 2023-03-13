6e945fb5654aa44c66921eb903f84fc6c472dca0
Samuael Ketema <lijsamuael@gmail.com>
2023-03-13 23:44:58 +0300

    (fea) printing arrays of texts using for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const list = [
-	'C is fun',
-	'Python is cool',
-	'JavaScript is amazing'
-];
-for(let i = 0; i < list.length; i++){
-	console.log(list[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
