d10a4a674d7f4c8ad89ebe16fd9a42cb56993f7a
Samuael Ketema <lijsamuael@gmail.com>
2023-03-14 22:00:30 +0300

    task 3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if(h > 0 && w > 0){
-    this.width = w;
-    this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
