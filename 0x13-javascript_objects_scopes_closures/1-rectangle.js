0021dc27b4643e818851da07b349caccacc586cb
Samuael Ketema <lijsamuael@gmail.com>
2023-03-14 21:54:48 +0300

    crating a class that has two constructors

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-class Rectangle{
-	constructor(w, h){
-		this.width = w;
-		this.height = h;
-	}
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
-module.exports = Rectangle
+module.exports = Rectangle;
